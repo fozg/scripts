e791c9904431aded7a9da19cd9ac012061d3d5c2
Fozg <phongdaohong@gmail.com>
2018-11-26 09:23:33 +0700

    Add Deploy Tomato service

--- fozg.net.sh
+++ fozg.net.sh
@@ -6,6 +6,7 @@ NC='\033[0m' # No Color
 list_cmd() {
   echo "----------------- select -----------------"
   echo -e "${RED}1:${NC} Restart Nginx"
+  echo -e "${RED}2:${NC} Deploy Tomato Server"
   echo -e "${RED}0:${NC} Exit"
   echo "You choose:"
 }
@@ -17,12 +18,21 @@ restart_nginx() {
   echo "Done"
 }
 
+deploy_tomato_server() {
+  echo "Pulling latest code..."
+  ssh root@fozg.net cd /home/tomato.fozg.net && git pull
+  echo "Restart tomato service"
+  ssh root@fozg.net pm2 restart tomato
+  echo "Done"
+}
+
 while [ "$INPUT_STRING" != "0" ]
 do 
   list_cmd
   read INPUT_STRING
   case $INPUT_STRING in
     1)		restart_nginx	;;
+    2)		deploy_tomato_server	;;
     0)		echo Bye	;;
     *)		echo "Please select a command";;
   esac
